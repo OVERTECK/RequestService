5bea6413c33d0eb87ce019496ecb600ec0698255
Korkin Alexsandr <korkin.06@inbox.ru>
2026-06-18 19:09:12 +0300

    Add: request status history

---
+++ TestTask/Domain/RequestStatusHistory.cs
@@ -0,0 +1,18 @@
+﻿namespace TestTask.Domain;
+
+public class RequestStatusHistory
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    public Guid RequestId { get; set; }
+    
+    public RequestStatus? FromStatus { get; set; }
+    
+    public RequestStatus ToStatus { get; set; }
+    
+    public string ChangedBy { get; set; } = default!;
+    
+    public string? Comment { get; set; }
+    
+    public DateTimeOffset ChangedAt { get; set; } = DateTimeOffset.UtcNow;
+}
