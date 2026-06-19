ebdc8208590efefe4e6e5219031391aea80a875c
Korkin Alexsandr <korkin.06@inbox.ru>
2026-06-19 21:14:42 +0300

    Add: request service interface

---
+++ TestTask/Service/IRequestService.cs
@@ -0,0 +1,20 @@
+﻿using TestTask.Domain;
+using TestTask.Dtos;
+
+namespace TestTask.Service;
+
+public interface IRequestService
+{
+    Task<(RequestResponseDto, bool created)> CreateAsync(
+        CreateRequestDto requestDto, string? idempotencyKey, CancellationToken ct);
+    
+    Task<IReadOnlyList<RequestResponseDto>> GetMineAsync(CancellationToken ct);
+    
+    Task<IReadOnlyList<RequestResponseDto>> GetQueueAsync(RequestStatus? status, CancellationToken ct);
+
+    Task<RequestResponseDto> GetByIdAsync(Guid id, CancellationToken ct);
+    
+    Task<RequestResponseDto> ChangeStatusAsync(Guid id, ChangeStatusDto dto, CancellationToken ct);
+    
+    Task<IReadOnlyList<StatusHistoryDto>> GetHistoryAsync(Guid id, CancellationToken ct);
+}
